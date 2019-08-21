e8e9a2c5808f80687605891ecbe438b405969aae
Brian Chang <changbhc@gmail.com>
2019-08-21 06:02:51 +0000

    completed exercise 6....haven't been comitting again, not great

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -1,9 +1,43 @@
+# frozen_string_literal: true
+
 require_relative '../setup'
 require_relative './exercise_1'
 require_relative './exercise_2'
 require_relative './exercise_3'
 
-puts "Exercise 4"
-puts "----------"
+puts 'Exercise 4'
+puts '----------'
 
 # Your code goes here ...
+
+Store.create(
+  name: 'Surrey',
+  annual_revenue: 224_000,
+  mens_apparel: false,
+  womens_apparel: true
+)
+
+Store.create(
+  name: 'Whistler',
+  annual_revenue: 1_900_000,
+  mens_apparel: true,
+  womens_apparel: false
+)
+
+Store.create(
+  name: 'Yaletown',
+  annual_revenue: 430_000,
+  mens_apparel: true,
+  womens_apparel: true
+)
+
+@men_stores = Store.where(mens_apparel: true, womens_apparel: false)
+@womens_stores = Store.where(mens_apparel: false, womens_apparel: true, annual_revenue: 0...1_000_000)
+@men_stores.each do |store|
+  puts store.name
+  puts store.annual_revenue
+end
+@womens_stores.each do |store|
+  puts store.name
+  puts store.annual_revenue
+end
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative '../setup'
 require_relative './exercise_1'
 require_relative './exercise_2'
@@ -5,7 +7,9 @@ require_relative './exercise_3'
 require_relative './exercise_4'
 require_relative './exercise_5'
 
-puts "Exercise 6"
-puts "----------"
+puts 'Exercise 6'
+puts '----------'
 
 # Your code goes here ...
+puts Employee.count
+@store1.employees.create(first_name: "Brian", last_name: "Chang", hourly_rate: 60)
